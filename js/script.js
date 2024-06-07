02ba267071f783ac54e71e6d6f6c5d8be76363a3
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2024-06-07 16:03:12 +0600

    added podcast

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function togglePlayPause() {
+    var audio1 = document.getElementById('audio1');
+    var icon1 = document.getElementById('playPauseIcon1');
+    if (audio1.paused) {
+      audio1.play();
+      icon1.classList.remove('fa-play');
+      icon1.classList.add('fa-pause');
+    } else {
+      audio1.pause();
+      icon1.classList.remove('fa-pause');
+      icon1.classList.add('fa-play');
+    }
+  }
